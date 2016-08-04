d97b3404348505ba915fc417354e4eb01fcf7155
Melissa Cabral <melissacabral@gmail.com>
2016-08-04 11:48:08 -0700

    after functions.php intro, added styling

--- functions.php
+++ functions.php
@@ -6,8 +6,72 @@ add_theme_support( 'post-thumbnails' );
 add_theme_support( 'post-formats', array( 'quote', 'image', 'gallery', 'audio', 'video', 'chat', 'aside', 'status', 'link' ) );
 
 				//name 	  width  height  crop?
-add_image_size( 'banner', 1200, 300, true );
-add_image_size( 'skinny-banner', 1200, 150, true );
+add_image_size( 'banner', 800, 200, true );
 //use force regenerate thumbnails to resize all your images!
+
+//wakes up panels in the 'customize' screen
+add_theme_support( 'custom-background' );
+
+//don't forget to add header_image() somewhere in your templates
+add_theme_support( 'custom-header', array(
+					'width' => 1200,
+					'height' => 400,
+					'flex-height' => true,
+				) );
+
+//put the_custom_logo() anywhere in your templates
+add_theme_support( 'custom-logo', array( 
+					'width' => 180,
+					'height' => 50,
+				 ) );
+
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
+//SEO-friendly <title> tags. delete <title>tag  from your header first!
+add_theme_support( 'title-tag' );
+
+//adds contextual RSS feeds for every tag, category, author, and post
+add_theme_support( 'automatic-feed-links' );
+
+//support for editor-style.css
+add_editor_style();
+
+
+/**
+ * Make the excerpt better
+ */
+add_filter( 'excerpt_length', 'awesome_ex_length' );
+
+function awesome_ex_length(){
+	//default is 55 words
+	//this condition will apply a different length on the search results
+	if( is_search() ){
+		return 25;
+	}else{
+		return 75;
+	}
+}
+
+//replace the [...] with a button
+add_filter( 'excerpt_more', 'awesome_ex_more' );
+function awesome_ex_more(){
+	return '&hellip; <a href="' . get_permalink() . '" class="button">Continue Reading</a>';
+}
+
+
+/**
+ * Improve Comment UX with Javascript
+ */
+add_action( 'wp_enqueue_scripts', 'awesome_comment_reply' );
+
+function awesome_comment_reply(){
+	if( is_single() AND comments_open() ){
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+
+
+
+
 
 //no close php!
--- header.php
+++ header.php
@@ -2,27 +2,39 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?php bloginfo('name'); ?> - <?php bloginfo( 'description' ); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" 
-		href="<?php echo get_stylesheet_directory_uri(); ?>/css/normalize.css">
-		
+	href="<?php echo get_stylesheet_directory_uri(); ?>/css/normalize.css">
+
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
 
 	<?php wp_head(); //hook. required for the admin bar and plugins to work ?>
 </head>
 <body <?php body_class(); ?>>
-	<header role="banner" id="header">
+
+	<header id="header" style="background-image:url(<?php header_image() ?>)">	
+		<div class="header-bar">
+		<?php 
+		//CUSTOM LOGO
+		if( function_exists( 'the_custom_logo' ) AND has_custom_logo() ){
+			the_custom_logo();
+		}else{ ?>
 		<h1 class="site-title"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+		<?php } ?>
 		<h2><?php bloginfo( 'description' ); ?></h2>
-		<nav>
-			<ul class="nav">
+
+
+		<nav class="menu">
+			<ul>
 				<?php wp_list_pages( array(
 					'depth' 	=> 1,
 					'title_li' 	=> '',
-					'exclude' 	=> '735,174', //list of page IDs to hide
-				) ); ?>
+					// 'exclude' 	=> '735,174', //list of page IDs to hide
+					) ); ?>
 			</ul>
 		</nav>
 
 		<?php get_search_form(); //include searchform.php OR WP's default search form ?>
+		</div>
 	</header>
+	<div class="wrapper">
